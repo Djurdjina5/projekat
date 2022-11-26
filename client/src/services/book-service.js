2445fa848ef6fea45207fb953361fa19f992194c
Djurdjina5 <djurdjinalozo@hotmail.com>
2022-11-26 22:05:55 +0100

    dodala dodavanje, zaduzivanje, vracanje knjige

--- client/src/services/book-service.js
+++ client/src/services/book-service.js
@@ -2,7 +2,33 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000/books";
 
-
+const book_upload = (title,publishedYear,authors,category,
+  numberOfPages,description, slika) => {
+  const authors_array = authors.split(",");
+  console.log("i am in book function")
+  var bodyFormData = new FormData();
+  bodyFormData.append('title', title);
+  bodyFormData.append('publishedYear',publishedYear);
+  bodyFormData.append('authors',authors_array);
+  bodyFormData.append('category',category);
+  bodyFormData.append('numberOfPages',numberOfPages);
+  bodyFormData.append('description',description);
+  bodyFormData.append('slika',slika);
+  return axios({
+    method: "post",
+    url: API_URL + "/storeBook",
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data" }
+  })
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });
+  
+};
 
 const get_free_books  = () => {
   return axios.get(
@@ -32,10 +58,46 @@ const get_books  = () => {
 
 }
 
+const loan_book  = (title) => {
+  const user_id = JSON.parse(localStorage.getItem('user'))._id;
+  return axios
+    .post(API_URL + "/loanBook", {
+      title,
+      user_id
+    })
+    .then((response) => {
+      return response.data;
+    }
+    )
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });
+
+}
+
+const return_book  = (title) => {
+  return axios
+    .post(API_URL + "/returnBook", {
+      title
+    })
+    .then((response) => {
+      return response.data;
+    }
+    )
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });;
+
+}
 
 const bookService = {
   get_free_books,
-  get_books
+  get_books,
+  book_upload,
+  loan_book,
+  return_book
  };
 
 export default bookService;
--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -32,3 +32,32 @@ module.exports.getFreeBooks = function(req,res)
 
 
 }
+
+module.exports.loanBook = function(req,res) {
+
+  console.log(req.body.title);
+  console.log(req.body.user_id);
+  Book.findOneAndUpdate({title:req.body.title},{isLoaned:true,loanedUser:req.body.user_id},function(err,doc){
+    if(err){
+      console.log("Error in loan book")
+      res.status(401).json({error:'Error in loan book'});
+    } else {
+      return res.status(200).json({message:'Book succesfully loaned'});
+    }
+
+  })
+}
+
+module.exports.returnBook = function(req,res) {
+  console.log("I am in return book")
+  console.log(req.body.title);
+  Book.findOneAndUpdate({title:req.body.title},{isLoaned:false,loanedUser:null},function(err,doc){
+    if(err){
+      console.log("Error in return book")
+      res.status(401).json({error:'Error in return book'});
+    } else {
+      return res.status(200).json({message:'Book succesfully returned'});
+    }
+
+  })
+}
