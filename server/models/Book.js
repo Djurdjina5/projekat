e7a8125b3cd885bf4f564ca69255fc8a2fc8a95b
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-12-28 22:25:19 +0100

    dodala kategoriju

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var BookSchema = new Schema(
-    {
-        title:{type:String},
-        yearPublished:{type:String},
-        authors:[{type:String}],
-        category:{type:String},
-        description:{type:String},
-        picturePath:{type:String},
-        numberOfPages:{type:Number},
-        isLoaned:{type:Boolean,default:false},
-        dateLoaned:{type:Date},
-        loanedUser:{type: Schema.Types.ObjectId, ref: 'User', default:null}
-    }
-);
-module.exports=mongoose.model("book", BookSchema);
+var BookSchema = new Schema({
+  title: { type: String },
+  yearPublished: { type: String },
+  authors: [{ type: String }],
+  category: { type: Schema.Types.ObjectId, ref: "category", default: null },
+  description: { type: String },
+  picturePath: { type: String },
+  numberOfPages: { type: Number },
+  isLoaned: { type: Boolean, default: false },
+  dateLoaned: { type: Date, default: null },
+  loanedUser: { type: Schema.Types.ObjectId, ref: "User", default: null },
+});
+module.exports = mongoose.model("book", BookSchema);
